d7a8ba40a95c74bdd490943d2b0d3a0bd3544b05
Larry Badmus <taofiq.badmus@gmail.com>
2025-08-20 18:17:34 -0400

    update codebuild config

--- variables.tf
+++ variables.tf
@@ -26,14 +26,17 @@ variable "github_pat_secret_name" {
 variable "vpc_id" {
   description = "The ID of the VPC to launch the CodeBuild project in."
   type        = string
+  default     = "vpc-0170011cde1ef44bd"
 }
 
 variable "subnet_ids" {
   description = "A list of subnet IDs for the CodeBuild project."
   type        = list(string)
+  default     = ["subnet-0d411569cb22d83b7", "subnet-04d78072fc48578ef"]
 }
 
 variable "security_group_ids" {
   description = "A list of security group IDs to associate with the CodeBuild project."
   type        = list(string)
+  default     = ["sg-08e9c2963f230a402"]
 }
